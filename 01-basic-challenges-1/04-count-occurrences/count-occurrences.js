9e98c6277e9f21f244c4ea1cea2a7c81d129c9b9
Illia <60040628+Barto-dev@users.noreply.github.com>
2024-02-28 20:54:21 +0000

    finish basic-challenge-4

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,26 @@
-function countOccurrences() {}
+/**
+ * Returns the number of occurrences of a character in a string.
+ * @param {string} str - The string to search.
+ * @param {string} char - The character to search for.
+ * @returns {number} - The number of occurrences of the character in the string.
+ */
+function countOccurrences1(str, char) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count++;
+    }
+  }
+  return count;
+}
+
+/**
+ * Returns the number of occurrences of a character in a string.
+ * @param {string} str - The string to search.
+ * @param {string} char - The character to search for.
+ * @returns {number} - The number of occurrences of the character in the string.
+ */
+const countOccurrences = (str, char) => str.split(char).length - 1;
+
 
 module.exports = countOccurrences;
