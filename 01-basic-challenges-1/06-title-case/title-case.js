f117b89a2bb0c6d4bf3b31d29234884cda27436b
Illia <60040628+Barto-dev@users.noreply.github.com>
2024-03-02 10:03:39 +0000

    finish basic-challenge-6

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,13 @@
-function titleCase() {}
+/**
+ * Returns a string with the first letter of each word capitalized.
+ * @param {string} str - The string to capitalize.
+ * @returns {string} - The string with the first letter of each word capitalized.
+ */
+function titleCase(str) {
+  return str
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ');
+}
 
 module.exports = titleCase;
