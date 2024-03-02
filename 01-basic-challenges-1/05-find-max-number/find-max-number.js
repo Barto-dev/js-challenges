9e86858cd36af635ebea49887c51b7d181e5d61d
Illia <60040628+Barto-dev@users.noreply.github.com>
2024-03-02 09:53:31 +0000

    finish basic-challenge-5

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,22 @@
-function findMaxNumber() {}
+/**
+ * Returns the largest number in an array.
+ * @param {number[]} arr - The array of numbers.
+ * @returns {number} - The largest number in the array.
+ */
+function findMaxNumber(arr) {
+  let maxNumber = arr[0];
+  // because we already select a first element
+  for (let i = 1; i < arr.length; i++) {
+    if (maxNumber < arr[i]) {
+      maxNumber = arr[i];
+    }
+  }
+  return maxNumber;
+}
+
+function findMaxNumberAlt(arr) {
+  return Math.max(...arr);
+}
+
 
 module.exports = findMaxNumber;
