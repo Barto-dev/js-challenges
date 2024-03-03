200af9aba19db3f3639a5cf4fc561f75a664d5cb
Illia <60040628+Barto-dev@users.noreply.github.com>
2024-03-03 09:38:02 +0000

    finish basic-challenge-7

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,22 @@
-function reverseString() {}
+/**
+ * Returns the reverse of a string.
+ * @param {string} str - The string to reverse.
+ * @returns {string} - The reverse of the string.
+ */
+function reverseString(str) {
+  let reversedStr = '';
+  for (let i = str.length; i > 0; i--) {
+    reversedStr += str[i - 1];
+  }
+  return reversedStr;
+}
+
+/**
+ * Returns the reverse of a string.
+ * @param {string} str - The string to reverse.
+ * @returns {string} - The reverse of the string.
+ */
+const reverseStringAlt = (str) => str.split('').reverse().join('');
+
 
 module.exports = reverseString;
