8ea6e29de5206828c6856aca62d5fc996a87d1f4
Illia <60040628+Barto-dev@users.noreply.github.com>
2024-03-08 14:38:02 +0000

    finish basic-challenge-10

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,36 @@
-function removeDuplicates() {}
+/**
+ * Returns a new array with duplicates removed.
+ * @param {any[]} arr - The array to remove duplicates from.
+ * @returns {any[]} - The new array with duplicates removed.
+ */
+function removeDuplicates(arr) {
+  return Array.from(new Set(arr));
+}
+
+/**
+ * Returns a new array with duplicates removed.
+ * @param {any[]} arr - The array to remove duplicates from.
+ * @returns {any[]} - The new array with duplicates removed.
+ */
+function removeDuplicates1(arr) {
+  return arr.filter((item, index) => arr.indexOf(item) === index);
+}
+
+/**
+ * Returns a new array with duplicates removed.
+ * @param {any[]} arr - The array to remove duplicates from.
+ * @returns {any[]} - The new array with duplicates removed.
+ */
+function removeDuplicatesAlt2(arr) {
+  const arrWithoutDuplicates = [];
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i];
+    const isUniq = !arrWithoutDuplicates.includes(item);
+    if (isUniq) {
+      arrWithoutDuplicates.push(item);
+    }
+  }
+  return arrWithoutDuplicates;
+}
 
 module.exports = removeDuplicates;
