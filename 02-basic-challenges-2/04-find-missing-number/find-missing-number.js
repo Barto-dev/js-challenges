8092e6c32f609d3a69c830920dc6f6867385fa86
Illia <60040628+Barto-dev@users.noreply.github.com>
2024-03-12 12:01:53 +0000

    finish basic-challenge-2-4

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,23 @@
-function findMissingNumber() {}
+/**
+ * Returns the missing number in an array of unique numbers from 1 to n (inclusive).
+ * @param {number[]} arr - The array of numbers.
+ * @returns {number} - The missing number.
+ */
+function findMissingNumber(arr) {
+  if (!arr) {
+    return undefined;
+  }
+
+  if (arr.length === 0) {
+    return 1;
+  }
+
+  const expectedLength = arr.length + 1;
+  // Gauss Summation
+  const expectedSum = expectedLength * (expectedLength + 1) / 2;
+  const actualSum = arr.reduce((acc, num) => acc + num, 0);
+
+  return expectedSum - actualSum;
+}
 
 module.exports = findMissingNumber;
