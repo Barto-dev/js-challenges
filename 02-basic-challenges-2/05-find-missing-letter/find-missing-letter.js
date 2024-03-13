a74c07be1a1187c0b6c53a6ee5f2d303ac11825c
Illia <60040628+Barto-dev@users.noreply.github.com>
2024-03-13 20:29:20 +0000

    finish basic-challenge-2-5

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,39 @@
-function findMissingLetter() {}
+const alphabetArr = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+
+/**
+ * Returns the missing letter in an array of consecutive letters.
+ * @param {string[]} arr - An array of consecutive letters.
+ * @returns {string} - The missing letter.
+ */
+function findMissingLetter(arr) {
+  const startCharCode = arr[0].charCodeAt(0);
+  for (let i = 1; i < arr.length; i++) {
+    const expectedCharCode = startCharCode + i;
+    if (arr[i].charCodeAt(0) !== expectedCharCode) {
+      return String.fromCharCode(expectedCharCode);
+    }
+  }
+  return '';
+}
+
+/**
+ * Returns the missing letter in an array of consecutive letters.
+ * @param {string[]} arr - An array of consecutive letters.
+ * @returns {string} - The missing letter.
+ */
+function findMissingLetterAlt(arr) {
+  if (!arr || !arr.length) {
+    return '';
+  }
+
+  const indexes = arr.map((item) => alphabetArr.indexOf(item));
+  const missedIndex = indexes.filter((item, index, arr) => {
+    return arr[index + 1] - item === 2;
+  })[0];
+  if (missedIndex) {
+    return alphabetArr[missedIndex + 1];
+  }
+}
 
 module.exports = findMissingLetter;
