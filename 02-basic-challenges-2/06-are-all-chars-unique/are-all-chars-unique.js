b2a83428e0e70a7c2cbf24f47a3d4e732ea34b01
Illia <60040628+Barto-dev@users.noreply.github.com>
2024-03-14 10:19:56 +0000

    finish basic-challenge-2-6

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,25 @@
-function areAllCharactersUnique() {}
+/**
+ * Returns true if all characters in a string are unique.
+ * @param {string} str - The string to check.
+ * @returns {boolean} - Whether all characters in the string are unique.
+ */
+// O(n)
+function areAllCharactersUnique(str) {
+  return new Set(str).size === str.length;
+}
+
+/**
+ * Returns true if all characters in a string are unique.
+ * @param {string} str - The string to check.
+ * @returns {boolean} - Whether all characters in the string are unique.
+ */
+// O(n^2) due to the use of indexOf inside the filter function
+function areAllCharactersUniqueALT(str) {
+  const nonUniqArr = str
+    .split('')
+    .filter((item, index) => str.indexOf(item) !== index);
+
+  return nonUniqArr.length === 0;
+}
 
 module.exports = areAllCharactersUnique;
