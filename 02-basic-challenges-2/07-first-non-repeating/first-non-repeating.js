4be10548952c628866f9a19bb36ee58594b59d00
Illia <60040628+Barto-dev@users.noreply.github.com>
2024-03-19 15:21:36 +0000

    finish basic-challenge-2-7

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,21 @@
-function findFirstNonRepeatingCharacter() {}
+/**
+ * Returns the first non-repeating character in a string.
+ * @param {string} str - The string to search.
+ * @returns {string | null} - The first non-repeating character in the string or null if there are no non-repeating characters.
+ */
+// O(n)
+function findFirstNonRepeatingCharacter(str) {
+  const charCount = new Map();
+  for (const char of str) {
+    charCount.set(char, (charCount.get(char) || 0) + 1);
+  }
+
+  for (const [char, count] of charCount) {
+    if (count === 1) {
+      return char;
+    }
+  }
+  return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
