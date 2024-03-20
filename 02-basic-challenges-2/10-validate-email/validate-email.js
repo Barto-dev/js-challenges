15754dfcb0d5f06468ad5522bc8f268e487a660a
Illia <60040628+Barto-dev@users.noreply.github.com>
2024-03-20 12:44:01 +0000

    finish basic-challenge-2-10

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,12 @@
-function validateEmail() {}
+const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+
+/**
+ * Returns whether the string is a valid email address.
+ * @param {string} email - The email address to validate.
+ * @returns {boolean} - Whether the email address is valid.
+ */
+function validateEmail(email) {
+  return emailRegex.test(email);
+}
 
 module.exports = validateEmail;
