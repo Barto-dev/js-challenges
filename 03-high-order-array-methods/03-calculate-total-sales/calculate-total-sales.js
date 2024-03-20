7ac9e243c4494be0ad7b2f018805220e9a071921
Illia <60040628+Barto-dev@users.noreply.github.com>
2024-03-20 13:40:26 +0000

    finish high-order-array-methods-3-2

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,17 @@
-function calculateTotalSalesWithTax() {}
+/**
+ * Calculates and returns the total sales amount including tax from the input array of products and tax rate.
+ * @param {Object[]} products - The array of product objects.
+ * @param {string} products[].name - The name of the product.
+ * @param {number} products[].price - The price of the product.
+ * @param {number} products[].quantity - The quantity sold of the product.
+ * @param {number} taxRate - The tax rate as a percentage.
+ * @returns {number} - The total sales amount including tax.
+ */
+function calculateTotalSalesWithTax(products, taxRate) {
+  const totalSales = products
+    .reduce((total, product) => product.price * product.quantity + total, 0);
+  const tax = totalSales / 100 * taxRate;
+  return tax + totalSales;
+}
 
 module.exports = calculateTotalSalesWithTax;
