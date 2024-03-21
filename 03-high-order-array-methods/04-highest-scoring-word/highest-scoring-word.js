b61d1fe7907429749261e4006ebca2fd6b1b0146
Illia <60040628+Barto-dev@users.noreply.github.com>
2024-03-21 21:01:24 +0000

    finish high-order-array-methods-3-3

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,18 @@
-function highestScoringWord() {}
+/**
+ * Returns the highest scoring word from a string.
+ * @param {string} str - The input string.
+ * @returns {string} - The highest scoring word.
+ */
+function highestScoringWord(str) {
+  const words = str.toLowerCase().split(' ');
+  const highestScoreWord = words.reduce((wordObj, word) => {
+    const wordScore = [...word].reduce((score, char) => char.charCodeAt(0) + score, 0);
+    if (wordScore > wordObj.score) {
+      return {word: word, score: wordScore};
+    }
+    return wordObj;
+  }, {word: '', score: 0});
+  return highestScoreWord.word;
+}
 
 module.exports = highestScoringWord;
