5e1fafc0e4866de068f6be4fed2ac97a653dc87d
Illia <60040628+Barto-dev@users.noreply.github.com>
2024-03-23 09:16:46 +0000

    finish high-order-array-methods-3-4

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,13 @@
-function validAnagrams() {}
+/**
+ * Determines whether two strings are valid anagrams.
+ * @param {string} str1 - The first string.
+ * @param {string} str2 - The second string.
+ * @returns {boolean} - True if the strings are valid anagrams, false otherwise.
+ */
+function validAnagrams(str1, str2) {
+  const sortedStr1 = str1.toLowerCase().split('').sort().join('');
+  const sortedStr2 = str2.toLowerCase().split('').sort().join('');
+  return sortedStr1 === sortedStr2;
+}
 
 module.exports = validAnagrams;
