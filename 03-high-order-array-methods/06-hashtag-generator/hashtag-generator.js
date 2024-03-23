974a2aa2dcf36dc8598fc8285538e7d04ac4764f
Illia <60040628+Barto-dev@users.noreply.github.com>
2024-03-23 09:31:40 +0000

    finish high-order-array-methods-3-6

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,20 @@
-function generateHashtag() {}
+/**
+ * Generates a hashtag from the input string.
+ * @param {string} str - The input string.
+ * @returns {string|boolean} - The generated hashtag string or false.
+ */
+function generateHashtag(str) {
+  if (!str.trim()) {
+    return false;
+  }
+
+  // trim whitespaces, tabs, new line chars
+  const hash = str.split(/\s+/).reduce((acc, word) => {
+    const hashWord = word.charAt(0).toUpperCase() + word.slice(1);
+    return acc += hashWord;
+  }, '#');
+
+  return hash.length > 140 ? false : hash;
+}
 
 module.exports = generateHashtag;
