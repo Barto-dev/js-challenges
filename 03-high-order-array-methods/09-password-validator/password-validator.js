6f734ea735a23e43c1cf50ac772922182877523e
Illia <60040628+Barto-dev@users.noreply.github.com>
2024-03-26 09:01:25 +0000

    finish high-order-array-methods-3-9

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,35 @@
-function validatePassword() {}
+/**
+ * Validates a password based on certain criteria.
+ * @param {string} password - The password to be validated.
+ * @returns {boolean} - True if the password is valid, false otherwise.
+ */
+function validatePassword(password) {
+  const isValidLength = password.length >= 8;
+  const hasNumber = /\d/.test(password);
+  const hasUppercaseLetter = password !== password.toLowerCase();
+  const hasLowercaseLetter = password !== password.toUpperCase();
+  return isValidLength && hasNumber && hasLowercaseLetter && hasUppercaseLetter;
+}
+
+/**
+ * Validates a password based on certain criteria.
+ * @param {string} password - The password to be validated.
+ * @returns {boolean} - True if the password is valid, false otherwise.
+ */
+function validatePasswordAlt(password) {
+  const passwordArr = password.split('');
+  const isValidLength = passwordArr.length >= 8;
+  const hasNumber = passwordArr.some((char) => {
+    return char.charCodeAt(0) >= 48 && char.charCodeAt(0) <= 57;
+  });
+  const hasUppercaseLetter = passwordArr.some((char) => {
+    return char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90;
+  });
+  const hasLowercaseLetter = passwordArr.some((char) => {
+    return char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122;
+  });
+
+  return isValidLength && hasNumber && hasLowercaseLetter && hasUppercaseLetter;
+}
 
 module.exports = validatePassword;
